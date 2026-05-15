e8f51e724d8b51304dc8d72c5e58940451be9fb0
ASUS <ASUS@LAPTOP-O9EAS0QT>
2026-05-15 20:01:16 +0530

    Immutable String

---
+++ src/com/java/core/StringDayThree/StringDemo.java
@@ -0,0 +1,23 @@
+package com.java.core.StringDayThree;
+
+public class StringDemo {
+	public static void main(String[] args) {
+//		literals->SCP->No Duplicates for same String
+		String s="Irise";
+		
+//		new keyword -> Heap Memory ->New string is created every time
+		String s1=new String("Irise");
+		
+//		equals-> data and content compare
+//		== -> objects compare /Memory location compare
+		
+//		s.length();-> to get length of string
+		
+		
+		
+		
+		
+		
+	}
+
+}
